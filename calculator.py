db69dba0784505528dab4cbe5bf92202d8a44278
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:42:27 -0700

    Streamlined float-ing of tokenized input

--- calculator.py
+++ calculator.py
@@ -9,36 +9,49 @@ from arithmetic import *
 
 # Your code goes here
 
+
 while True:
     input = raw_input()
     tokenized_input = input.split(" ")
+
+    num1 = float(tokenized_input[1])
+    try:
+        tokenized_input[2]
+        num2 = float(tokenized_input[2])
+    except:
+        pass
+
+    # for item in tokenized_input:
+    #     if item.index(0) > 0:
+    #         float(item)
+
     if tokenized_input[0] == "q" or tokenized_input[0] == "quit":
         #this will exit calculator
         break
     elif tokenized_input[0] == "+":
         #arithmetic add function
-        print add(float(tokenized_input[1]), float(tokenized_input[2]))
+        print add(num1, num2)
     elif tokenized_input[0] == "-":
         #arithmetic subtract function
-        print subtract(float(tokenized_input[1]), float(tokenized_input[2]))
+        print subtract(num1, num2)
     elif tokenized_input[0] == "*":
         #arithmetic multiply function
-        print multiply(float(tokenized_input[1]), float(tokenized_input[2]))
+        print multiply(num1, num2)
     elif tokenized_input[0] == "/":
         #arithmetic divide function
-        print divide(float(tokenized_input[1]), float(tokenized_input[2]))
+        print divide(num1, num2)
     elif tokenized_input[0] == "square":
         #arithmetic square function
-        print square(float(tokenized_input[1]))
+        print square(num1)
     elif tokenized_input[0] == "cube":
         #arithmetic cube function
-        print cube(float(tokenized_input[1]))
+        print cube(num1)
     elif tokenized_input[0] == "pow":
         #arithmetic power function
-        print power(float(tokenized_input[1]), float(tokenized_input[2]))
+        print power(num1, num2)
     elif tokenized_input[0] == "mod":
         #arithmetic modulo function
-        print mod(float(tokenized_input[1]), float(tokenized_input[2]))
+        print mod(num1, num2)
     else:
         print "I do not understand!"
 
